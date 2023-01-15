3c9dc69e1bfb4ef0436215dee3ba1e5a3a23ea8d
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-15 14:52:00 -0300

    add: header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/landing-page/landing-page'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <Routes>
--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -1,5 +1,17 @@
+import Logo from '../../assets/logo.png'
+
 export default function LandingPage() {
   return(
-    <div>hello world</div>
+    <div>
+      <header>
+        <img src={Logo} alt="logo" />
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Sobre nós</a>
+          <a href="#">Serviço</a>
+          <a href="#">Contato</a>
+        </nav>
+      </header>
+    </div>
   );
 }
