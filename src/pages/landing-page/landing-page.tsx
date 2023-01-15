70078b68b1dc25fa077be35942bef74a552c8f2c
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-15 15:09:36 -0300

    add: home section

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -1,4 +1,10 @@
 import Logo from '../../assets/logo.png'
+import Technician from '../../assets/technician.svg';
+import Location from '../../assets/icons/location.svg';
+import Services from '../../assets/icons/services.svg';
+import Clients from '../../assets/icons/clients.svg';
+import Attendance from '../../assets/icons/attendance.svg';
+
 
 export default function LandingPage() {
   return(
@@ -12,6 +18,30 @@ export default function LandingPage() {
           <a href="#">Contato</a>
         </nav>
       </header>
+      <main>
+        <div>
+          <img src={Technician} alt="técnico " />
+          <div>
+            <h1>Conserto de máquinas lava e seca em um só lugar!</h1>
+            <span>
+              <img src={Location} alt="logo de localização" />
+              <p>São Paulo, capital</p>
+            </span>
+            <span>
+              <img src={Services} alt="logo de serviços" />
+              <p>10+ serviços</p>
+            </span>
+            <span>
+              <img src={Clients} alt="logo de clientes" />
+              <p>50+ clientes</p>
+            </span>
+            <span>
+              <img src={Attendance} alt="logo de atendimento" />
+              <p>50+ atendimentos</p>
+            </span>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
