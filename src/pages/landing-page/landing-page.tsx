21ec3ff1ed9eee035b2e6bf0f929c13dfcefd163
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-01-18 16:10:31 -0300

    style: footer section

--- src/pages/landing-page/landing-page.tsx
+++ src/pages/landing-page/landing-page.tsx
@@ -13,6 +13,7 @@ import Leaking from '../../assets/icons/leaking.svg';
 import Email from '../../assets/icons/email.svg';
 import Whatsapp from '../../assets/icons/whatsapp.svg';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineCopyright } from 'react-icons/ai';
 
 import Service from './components/Service';
 import Status from './components/Status';
@@ -68,20 +69,31 @@ export default function LandingPage() {
 
         </section>
       </main>
-      <footer className="bg-background-color ">
-        <h1>Contato</h1>
-        <div>
-          <p><strong>Atendimento</strong>: seg à sex, das 9h as 18h</p>
-          <span>
-            <img src={Email} alt="email logo" />
-            E-mail: flavio.consertomaquinas@gmail.com
-          </span>
-          <span>
-            <img src={Whatsapp} alt="email logo" />
-            Whatsapp:  +55 (11) 99999-9999
-          </span>
+      <footer>
+        <div className="bg-background-color">
+          <h1 className="text-3xl text-primary-color text-center">Contato</h1>
+          <p className="text-primary-color text-lg text-center px-3">Nos contate para fazer um agendamento de visita técnica para resolver seu problema!</p>
+          <div className="m-3">
+            <p className="mb-3"><strong>Atendimento</strong>: seg à sex, das 9h as 18h</p>
+            <span className="flex">
+              <img src={Email} alt="email logo" className="mr-2" />
+              <p>E-mail: flaviomaquinas@gmail.com</p>
+            </span>
+            <span className="flex pb-3">
+              <img src={Whatsapp} alt="email logo" className="mr-2" />
+              <p>Whatsapp: (11) 99999-9999</p>
+            </span>
+
+          </div>
         </div>
-        <p>Entre em contato conosco, agenda uma visita técnica para resolver seu problema agora!</p>
+        <div>
+          <h1 className="text-center text-2xl my-3">Privacidade e termos de serviços</h1>
+          <span className="flex flex-col items-center mt-10">
+            <img src={Logo} alt="logo" className="w-[10em]" />
+            <p className="flex items-center justify-center">2023 <AiOutlineCopyright className="mx-1" /> Todos os direitos reservados</p>
+          </span>
+
+        </div>
       </footer>
     </div>
   );
