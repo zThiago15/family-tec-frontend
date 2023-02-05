d3a4f2a74490cafed6ac52755857465f0195df49
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-02-05 10:41:48 -0300

    feat: implement fetch of 5 top feedbacks

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -5,3 +5,8 @@ export const createFeedback = async (data: IFeedback) => {
     const response = await api.post('/feedback', {...data});
     return response;
 }
+
+export const getTopFiveFeedbacks = async () => { 
+    const response = await api.get('/feedbacktop5')
+    return response;
+}
